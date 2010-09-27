daee54a5d8c9c512406857bc85af14ec867c73f7
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-09-27 10:46:52 -0700

    Adding test for Month#month

--- spec/distance_between_dates/month_spec.rb
+++ spec/distance_between_dates/month_spec.rb
@@ -52,5 +52,59 @@ describe "DistanceBetweenDates" do
       @month.should be_less_than( DistanceBetweenDates::Year.new 2011 )
       @month.should be_greater_than( DistanceBetweenDates::Year.new 2009 )
     end
+
+    it "has month names" do 
+      DistanceBetweenDates::Month.names.include? 'january'
+      DistanceBetweenDates::Month.names.include? 'february'
+      DistanceBetweenDates::Month.names.include? 'march'
+      DistanceBetweenDates::Month.names.include? 'april'
+      DistanceBetweenDates::Month.names.include? 'may'
+      DistanceBetweenDates::Month.names.include? 'june'
+      DistanceBetweenDates::Month.names.include? 'july'
+      DistanceBetweenDates::Month.names.include? 'august'
+      DistanceBetweenDates::Month.names.include? 'september'
+      DistanceBetweenDates::Month.names.include? 'october'
+      DistanceBetweenDates::Month.names.include? 'november'
+      DistanceBetweenDates::Month.names.include? 'december'
+
+      DistanceBetweenDates::Month.names.include? 'January'
+      DistanceBetweenDates::Month.names.include? 'February'
+      DistanceBetweenDates::Month.names.include? 'March'
+      DistanceBetweenDates::Month.names.include? 'April'
+      DistanceBetweenDates::Month.names.include? 'May'
+      DistanceBetweenDates::Month.names.include? 'June'
+      DistanceBetweenDates::Month.names.include? 'July'
+      DistanceBetweenDates::Month.names.include? 'August'
+      DistanceBetweenDates::Month.names.include? 'September'
+      DistanceBetweenDates::Month.names.include? 'October'
+      DistanceBetweenDates::Month.names.include? 'November'
+      DistanceBetweenDates::Month.names.include? 'December'
+
+      DistanceBetweenDates::Month.names.include? 'jan'
+      DistanceBetweenDates::Month.names.include? 'feb'
+      DistanceBetweenDates::Month.names.include? 'mar'
+      DistanceBetweenDates::Month.names.include? 'apr'
+      DistanceBetweenDates::Month.names.include? 'may'
+      DistanceBetweenDates::Month.names.include? 'jun'
+      DistanceBetweenDates::Month.names.include? 'jul'
+      DistanceBetweenDates::Month.names.include? 'aug'
+      DistanceBetweenDates::Month.names.include? 'sep'
+      DistanceBetweenDates::Month.names.include? 'oct'
+      DistanceBetweenDates::Month.names.include? 'nov'
+      DistanceBetweenDates::Month.names.include? 'dec'
+
+      DistanceBetweenDates::Month.names.include? 'Jan'
+      DistanceBetweenDates::Month.names.include? 'Feb'
+      DistanceBetweenDates::Month.names.include? 'Mar'
+      DistanceBetweenDates::Month.names.include? 'Apr'
+      DistanceBetweenDates::Month.names.include? 'May'
+      DistanceBetweenDates::Month.names.include? 'Jun'
+      DistanceBetweenDates::Month.names.include? 'Jul'
+      DistanceBetweenDates::Month.names.include? 'Aug'
+      DistanceBetweenDates::Month.names.include? 'Sep'
+      DistanceBetweenDates::Month.names.include? 'Oct'
+      DistanceBetweenDates::Month.names.include? 'Nov'
+      DistanceBetweenDates::Month.names.include? 'Dec'
+    end
   end
 end
