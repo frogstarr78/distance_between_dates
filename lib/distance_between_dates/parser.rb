3c8bd21b428fda137dde75d8e1079238d078758a
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-10-05 05:03:19 -0700

    Aadded some code to handle what I'm not testing

--- lib/distance_between_dates/parser.rb
+++ lib/distance_between_dates/parser.rb
@@ -3,36 +3,43 @@ module DistanceBetweenDates
     class << self
       def parse argv
         while arg = argv.shift
-          if arg == 'from'
+          case arg
+          when 'from', 'between'
             from_month = extract_values argv
-          elsif arg == 'to'
+          when 'to', 'and'
             to_month = extract_values argv
+          when 'in'
+            date_parts = extract_date_parts argv
           end
         end
 
-        return [from, to]
+        return [from_month, to_month]
       end
 
-      def extract_year_values year, args 
-        if year == 'from' or year == 'to'
+      def extract_values args
+        day   = args.shift
+        if day[0,1] == '"'
+          day, month, year = day.split ' '
+          year = conditionally_reset_year year, args
+        else
+          month = args.shift
+          year  = args.shift
+          year = conditionally_reset_year year, args
+        end
+
+        Day.new day, month, year
+      end
+
+      def conditionally_reset_year year, args 
+        if year == 'from' or year == 'to' or year == 'in'
           args.unshift year
           year = now.year
         end
         year ||= now.year
       end
 
-      def extract_values args
-          day   = args.shift
-          if day[0,1] == '"'
-            day, month, year = day.split ' '
-            year = extract_year_values year, args
-          else
-            month = args.shift
-            year  = args.shift
-            year = extract_year_values year, args
-          end
-          day = day.to_i
-          new day, month, year
+      ALLOWED_DATE_PARTS = %w(days weeks months quarters years decades generations life\ times ages centuries millenia epochs eons)
+      def extract_date_parts args
       end
     end
   end
